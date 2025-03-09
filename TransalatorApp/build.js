7be99b7ad397481e00770eb21b27d8c123a3e0bb
Cara Davies <cdav0019@student.monash.edu>
2025-03-09 14:59:05 +1100

    Fix build script to use ES modules
    Fix Netifly deploy

--- TransalatorApp/build.js
+++ TransalatorApp/build.js
@@ -1,9 +1,14 @@
-const fs = require('fs');
-const path = require('path');
+import { readFileSync, writeFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Read the translator.js file
-const translatorPath = path.join(__dirname, 'js', 'pages', 'translator.js');
-let content = fs.readFileSync(translatorPath, 'utf8');
+const translatorPath = join(__dirname, 'js', 'pages', 'translator.js');
+let content = readFileSync(translatorPath, 'utf8');
 
 // Replace the API key placeholder with the actual value
 content = content.replace(
@@ -12,4 +17,4 @@ content = content.replace(
 );
 
 // Write the modified content back
-fs.writeFileSync(translatorPath, content); 
+writeFileSync(translatorPath, content); 
